68f0f3edb8474a7c6195b960b6297e4605e012f3
carlinhoshk <carlosmdohk@gmail.com>
2022-05-29 18:38:59 -0300

    exercicio3

---
+++ 3capitulo/Exericio3.sh
@@ -0,0 +1,22 @@
+#16:45 - 29/05/2022
+
+1º echo 1 million dolars... \\and that a bargain !
+
+2º LONG_STRING='1 million dolars... \\and that a bargain!'
+
+# O Comando banner não é mais usado nas versões mais atualizadas é preciso instalar
+3º banner good day  # Quebra linha em cada frase
+   banner "good day" # Não quebra frase em linha, provalmente usando "\"
+
+4º echo '$ABC' # Mostra uma string escrito $ABC
+   echo "$ABC" # Mostra uma Variavel sem nada dentro
+
+5º ASPA_DUPLA="" # Na hora de atribuir só é possivel salvar fechando as aspas.
+
+6º echo "Este caractere anula o significado do caractere apos ele:
+\ Este par anula o significado de todos por ele envolvidos: '
+E este aqui anula de todos, exceto \,$,{}, e ": "   "
+
+
+
+
