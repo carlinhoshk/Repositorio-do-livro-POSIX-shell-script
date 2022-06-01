1c0cd36a692a1289dee52ff382b27a5be2f0e8dd
carlinhoshk <carlosmdohk@gmail.com>
2022-06-01 04:03:18 -0300

    subindo exercicios do capitulo 4 do livro

---
+++ 4capitulo/myshellprog.sh
@@ -0,0 +1,7 @@
+echo "primeira resposta: $#"
+echo "segunda resposta: $3"
+echo "terceira resposta: $7"
+echo "quarta resposta: $*"
+echo "quinta resposta: $0"
+
+
---
+++ 4capitulo/nome_inverso.sh
@@ -0,0 +1,4 @@
+
+echo "Digite seu nome completo: "
+read nome
+echo $nome | rev   # esse comando "rev" não estava no livro eu achei e apliquei
