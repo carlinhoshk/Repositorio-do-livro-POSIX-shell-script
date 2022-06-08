4557c3bfb4a357a0c6e138ae268b2204c1e115d1
carlos Oliveira <40872405+carlinhoshk@users.noreply.github.com>
2022-06-08 15:16:13 -0300

    Update Exercicio1.sh

--- 7capitulo/Exercicio1.sh
+++ 7capitulo/Exercicio1.sh
@@ -4,4 +4,4 @@ while true;do
 	echo "Enviando menssagem para tela a cada 3 segundos"
 done
 
-# Ao ser executado o programa acima não respondera aos sinais HUP, INT, TERM, porem o sinal QUIT ainda está ativo. Portando, para interromper o programa basta pressionar a combinação de teclas ^\ ( CONTROL+BARRA INVERÇA ) isso para o script e gera um arquivo com o nome core.
+# Ao ser executado o programa acima não respondera aos sinais HUP, INT, TERM, porem o sinal QUIT ainda está ativo. Portando, para interromper o programa basta pressionar a combinação de teclas ^\ ( CONTROL+BARRA INVERSA ) isso para o script e gera um arquivo com o nome core.
